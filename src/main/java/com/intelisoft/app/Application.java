135e3e5253c90bce1bd11370c8118bd1e4b5104b
wasabi <wasa-bi@mail.ru>
2017-07-31 02:40:36 +0300

    Created Hibernate configurations.

---
+++ src/main/java/com/intelisoft/app/Application.java
@@ -0,0 +1,15 @@
+package com.intelisoft.app;
+
+import org.hibernate.SessionFactory;
+
+import com.intelisoft.util.HibernateUtil;
+
+public class Application {
+
+	public static void main(String[] args) {
+
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+
+	}
+
+}
---
+++ src/main/java/com/intelisoft/util/HibernateUtil.java
@@ -0,0 +1,41 @@
+package com.intelisoft.util;
+
+import org.apache.log4j.Logger;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernateUtil {
+
+	private static final Logger log = Logger.getLogger(HibernateUtil.class);
+
+	private static SessionFactory sessionFactory;
+    
+    private static SessionFactory buildSessionFactory() {
+        try {
+
+            Configuration configuration = new Configuration();
+            configuration.configure("hibernate.cfg.xml");
+            log.info("Hibernate Configuration loaded");
+             
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+            log.info("Hibernate serviceRegistry created");
+             
+            SessionFactory sessionFactory = configuration
+                                .buildSessionFactory(serviceRegistry);
+             
+            return sessionFactory;
+        }
+        catch (Throwable ex) {
+        	log.error("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+     
+    public static SessionFactory getSessionFactory() {
+        if(sessionFactory == null) sessionFactory = buildSessionFactory();
+        return sessionFactory;
+    }
+	
+}
